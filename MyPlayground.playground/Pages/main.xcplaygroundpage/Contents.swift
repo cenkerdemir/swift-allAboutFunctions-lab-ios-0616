a7c6c5f59896fedd90d167ffe60dc986d0e7ce19
github username <github email address>
2016-07-05 16:55:28 -0400

    completed

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,9 +14,11 @@
  ### 1. Create a function named `frozen` which takes no arguments. When this function is called, it prints "Let it go!".
  */
 // write your code here
+func frozen () {
+    print("Let it go!")
+}
 
-
-
+frozen()
 
 
 /*: question2
@@ -24,40 +26,56 @@
  */
 // write your code here
 
+func frozenAgain() {
+    let letsPrint = "Let it go, again!"
+    print("\(letsPrint)")
+}
 
-
-
+frozenAgain()
 /*: question3
  ### 3. Write a function that takes in a character's name as an argument (it can be any character from anything). What should the type of that argument be? Print the message "My favorite character is <character name>." to the screen.
  */
 // write your code here
 
 
+func characterName(name: String) {
+    print("My favorite character is \(name).")
+}
 
-
+characterName("Keloglan")
 /*: question4
  ### 4. Call the function you wrote in Question 3 using a constant you define. Then call it using a variable. Change the value of the variable, and call it again. What do you see in the console?
  */
 // write your code here
 
+let myCharacter = "Sahmaran"
+characterName(myCharacter)
 
-
-
+var yetAnother = "pismis kelle"
+characterName(yetAnother)
+yetAnother = "cevat kelle"
+characterName(yetAnother)
 /*: question5
  ### 5. Write a function that takes an integer as an argument and prints the string "I got <number> problems but Swift ain't one" to the console.
  */
 // write your code here
+func problems (numberOfProblems: Int) {
 
+    print("I got \(numberOfProblems) problems but Swift ain't one")
+}
 
-
+problems(32)
 
 /*: question6
  ### 6. Write a function that takes two arguments, the name of a band (a `String`) and a number (an `Int`). It should print the message "My #<number> favorite band is <band>." to the console.
  */
 // write your code here
 
+func bandNumber(band: String, number: Int) {
+    print("My #\(number) favorite band is \(band).")
+}
 
-
+bandNumber("Nirvana", number: 1)
 
 /*: question7
  ### 7. The code below is broken. Can you identify which line has an error and fix it so that it works again?
@@ -66,7 +84,7 @@ func badFavoriteBand(bandName: String, position: Int) {
     print("My #\(position) favorite band is \(bandName).")
 }
 
-badFavoriteBand("The Beatles", 2)
+badFavoriteBand("The Beatles", position: 2)
 
 
 
@@ -74,7 +92,7 @@ badFavoriteBand("The Beatles", 2)
 /*: question8
  ### 8. This code is broken, too. Assume the call to the function is correct. What's broken about the function definition? Can you fix it?
  */
-func alsoBadFavoriteBand(bandName: String, position: String) {
+func alsoBadFavoriteBand(bandName: String, position: Int) {
     print("My #\(position) favorite band is \(bandName)")
 }
 
@@ -88,15 +106,19 @@ alsoBadFavoriteBand("Blink-182", position: 42)
  */
 // write your code here
 
+func madLib(name: String, noun: String, preposition: String) {
+    print("To \(noun) and \(preposition), \(name)!")
+}
 
-
-
+madLib("Fox", noun: "table", preposition: "above")
 /*: question10
  ### 10. Create a function that takes no arguments and returns the string "Buzz Lightyear to the rescue!"
  */
 // write your code here
-
-
+func rescue() -> String {
+    return "Buzz Lightyear to the rescue!"
+}
+rescue()
 
 
 /*: question11
@@ -104,17 +126,21 @@ alsoBadFavoriteBand("Blink-182", position: 42)
  */
 // write your code here
 
+func anyNumber() -> Int {
+    return 100
+}
 
-
-
+anyNumber()
 /*: question12
  ### 12. Create a function that takes in a characters name. This function will return back a `String` as follows: "To infinity and beyond, <character name>!". The character name should be returned uppercased.
  */
 // write your code here
 
+func infinityFunction(character: String) -> String {
+    return "To infinity and beyond, \(character)!"
+}
 
-
-
+infinityFunction("Foxy Lady")
 /*:
  [Solution](solution)
  */
